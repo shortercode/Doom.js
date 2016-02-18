c5fc3766c92b4af61056a74b4f3133a5c7fa2708
shortercode <iainshorter@gmail.com>
2016-02-18 15:00:54 +0000

    Allows selector string values for remove

--- src/RemoveElement.js
+++ src/RemoveElement.js
@@ -1,8 +1,20 @@
 Doom.remove = function (element, delay){
     if (delay) {
         setTimeout(Doom.remove, delay, element);
-    } else if (element.parentNode) {
-		element.parentNode.removeChild(element);
+    } else {
+        if (typeof element === 'string') {
+            element = Doom.search(element);
+        }
+        if (element[0]) {
+            var i = element.length;
+            while (i--) {
+                Doom.remove(element[i]);
+            }
+            return element;
+        }
+        if (element.parentNode) {
+            element.parentNode.removeChild(element);
+        }
 	}
     return element;
 };
